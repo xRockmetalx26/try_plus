369cbc67a9191375a9ebac83d9aa3af3f07ae8fc
Ronald <rockmetal281@gmail.com>
2025-06-02 10:06:34 -0400

    ajustes de TryError

--- lib/src/errors.dart
+++ lib/src/errors.dart
@@ -2,9 +2,7 @@ import 'package:either_plus/either_plus.dart' show EitherError;
 
 final class TryError extends EitherError {
   const TryError(
-    super.message, {
-    super.details,
-    super.code,
+    String message, {
     super.exception,
-  });
+  }) : super(message: message);
 }
